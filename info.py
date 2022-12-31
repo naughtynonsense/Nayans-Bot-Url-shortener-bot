5eb7132e93ae996b0181728855f95ef83eadf123
naughtynonsense <84988811+naughtynonsense@users.noreply.github.com>
2022-12-31 07:19:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,8 +26,8 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '739667270').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL', '-1001528080426')
-auth_grp = environ.get('AUTH_GROUP', '-1001422937091')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001565856771')
+auth_grp = environ.get('AUTH_GROUP', '-1001694316223')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
